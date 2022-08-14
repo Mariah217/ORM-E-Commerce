5c2aa18999a7bd88621eb6e2f5e7d7535b5eaa65
Mariah <mariahjohnston217@outlook.com>
2022-08-13 21:09:42 -0400

    added onUpdate cascade

--- models/index.js
+++ models/index.js
@@ -7,21 +7,25 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
-  onDelete: 'CASCADE'
+  onDelete: 'CASCADE',
+  onUpdate: 'CASCADE'
 })
 // Categories have many Products
 Category.hasMany(Product,{
-  foreignKey: 'category_id'
+  foreignKey: 'category_id',
+  onUpdate: 'CASCADE'
 })
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag,{
   through: ProductTag,
-  foreignKey: 'product_id'
+  foreignKey: 'product_id',
+  onUpdate: 'CASCADE'
 })
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  foreignKey: 'tag_id'
+  foreignKey: 'tag_id',
+  onUpdate: 'CASCADE'
 })
 
 module.exports = {
